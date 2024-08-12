ce422c64c4a465df947e4f3bc5a5307fc91166aa
sprinklepuffz <lovehenny23@gmail.com>
2024-08-12 16:28:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-function sendMail() {
-    let params = {
-        name: document.getElementById("demo-name").value,
-        email: document.getElementById("demo-email").value,
-        message: document.getElementById("demo-message").value,
-    };
-    var recaptchaResponse = grecaptcha.getResponse();
-    if (recaptchaResponse.length === 0) {
-        alert("Please complete the reCAPTCHA.");
-        return; // Prevent form submission
-    }
+grecaptcha.ready(function() {
+    grecaptcha.execute();
+});
 
-    emailjs.send("service_0f734hc", "template_fzlymrv", params)
-        .then(function(response) {
-            document.getElementById('notification').innerHTML = '<p>Form Submitted</p>';
-            document.getElementById('notification').style.display = 'block';
-            document.getElementById('notification').style.color = 'green';
-        })
-        .catch(function(error) {
-            document.getElementById('notification').innerHTML = '<p>Failed to send email. Please try again.</p>';
-            document.getElementById('notification').style.display = 'block';
-            document.getElementById('notification').style.color = 'red';
+function sendMail() {
+    grecaptcha.ready(function() {
+        grecaptcha.execute().then(function(token) {
+            let params = {
+                name: document.getElementById("demo-name").value,
+                email: document.getElementById("demo-email").value,
+                message: document.getElementById("demo-message").value,
+                'g-recaptcha-response': token // Use the token here
+            };
+            
+            emailjs.send("service_0f734hc", "template_fzlymrv", params)
+                .then(function(response) {
+                    document.getElementById('notification').innerHTML = '<p>Form Submitted</p>';
+                    document.getElementById('notification').style.display = 'block';
+                    document.getElementById('notification').style.color = 'green';
+                })
+                .catch(function(error) {
+                    document.getElementById('notification').innerHTML = '<p>Failed to send email. Please try again.</p>';
+                    document.getElementById('notification').style.display = 'block';
+                    document.getElementById('notification').style.color = 'red';
+                });
         });
+    });
 }
 
 
