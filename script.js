2ce40d4d08b788cec3d7e9b9549af244a336a6ff
sprinklepuffz <lovehenny23@gmail.com>
2024-08-12 16:46:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,26 +29,36 @@ function sendMail() {
 }
 
 
-function sendMail2() {
-    let params = {
-        name: document.getElementById("demo-name").value,
-        email: document.getElementById("demo-email").value,
-        message: document.getElementById("demo-message").value,
-    };
 
-    emailjs.send("service_0f734hc", "template_3h1i278", params)
-        .then(function(response) {
-            document.getElementById('notification').innerHTML = '<p>Form Submitted</p>';
-            document.getElementById('notification').style.display = 'block';
-            document.getElementById('notification').style.color = 'green';
-        })
-        .catch(function(error) {
-            document.getElementById('notification').innerHTML = '<p>Failed to send email. Please try again.</p>';
-            document.getElementById('notification').style.display = 'block';
-            document.getElementById('notification').style.color = 'red';
+
+
+
+function sendMail2() {
+    grecaptcha.ready(function() {
+        grecaptcha.execute().then(function(token) {
+            let params = {
+                name: document.getElementById("demo-name").value,
+                email: document.getElementById("demo-email").value,
+                message: document.getElementById("demo-message").value,
+                'g-recaptcha-response': token
+            };
+
+            emailjs.send("service_0f734hc", "template_3h1i278", params)
+                .then(function(response) {
+                    document.getElementById('notification').innerHTML = '<p>Form Submitted</p>';
+                    document.getElementById('notification').style.display = 'block';
+                    document.getElementById('notification').style.color = 'green';
+                })
+                .catch(function(error) {
+                    document.getElementById('notification').innerHTML = '<p>Failed to send email. Please try again.</p>';
+                    document.getElementById('notification').style.display = 'block';
+                    document.getElementById('notification').style.color = 'red';
+                });
         });
+    });
 }
 
 
+=
 
 
