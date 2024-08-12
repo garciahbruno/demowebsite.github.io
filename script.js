b6e295df8e2b784e82a9406a197c8963fd4eb107
sprinklepuffz <lovehenny23@gmail.com>
2024-08-11 22:21:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,26 @@ function sendMail() {
             document.getElementById('notification').style.color = 'red';
         });
 }
+function sendMail2() {
+    let params = {
+        name: document.getElementById("demo-name").value,
+        email: document.getElementById("demo-email").value,
+        message: document.getElementById("demo-message").value,
+    };
+
+    emailjs.send("service_0f734hc", "template_3h1i278", params)
+        .then(function(response) {
+            document.getElementById('notification').innerHTML = '<p>Form Submitted</p>';
+            document.getElementById('notification').style.display = 'block';
+            document.getElementById('notification').style.color = 'green';
+        })
+        .catch(function(error) {
+            document.getElementById('notification').innerHTML = '<p>Failed to send email. Please try again.</p>';
+            document.getElementById('notification').style.display = 'block';
+            document.getElementById('notification').style.color = 'red';
+        });
+}
+
 
 
 
